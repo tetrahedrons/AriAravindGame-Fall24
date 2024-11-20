76344d62ec91fe497d73700a81db0d87a22a8f8c
tetrahedrons <ari.aravindh15@gmail.com>
2024-11-19 18:29:17 -0800

    movement

---
+++ Assets/IJKL.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class IJKL : MonoBehaviour
+{
+    [SerializeField] float speed;
+    int x;
+    int y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        y = 0; x = 0; speed = 2 ;
+
+        if (Input.GetKey(KeyCode.I))
+            {
+               y = y + 1;
+            }
+
+        if (Input.GetKey(KeyCode.K))
+            {
+               y = y - 1;
+            }
+
+        if (Input.GetKey(KeyCode.L))
+            {
+               x = x + 1;
+            }
+
+        if (Input.GetKey(KeyCode.J))
+            {
+               x = x - 1;
+            }
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(pos.x + x * speed * Time.deltaTime, pos.y + y * speed * Time.deltaTime, pos.z);
+
+    }
+}
